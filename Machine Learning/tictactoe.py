98804a2489cd85a24c55454beecf8a751cbe9fa6
MEGADragon2.0 <106551997+MEGADragon20@users.noreply.github.com>
2024-06-03 20:03:21 +0200

    Update tictactoe.py

--- Machine Learning/tictactoe.py
+++ Machine Learning/tictactoe.py
@@ -54,17 +54,48 @@ def medium_gamemode(field):
         return easy_gamemode(field)
 
 def hard_gamemode(field):
+    def minimax(board, depth, maxen):
+        if gameOver(board):
+            return evaluateBoard(board)
 
+        if maxen == True:
+            bScore = -9999999999999999999999
+            for i in availableMoves(board):
+                make_move(board, "x" , i)
+                score = minimax(board, depth+1, False)
+                undoMove(board, i)
+                best_score = max(score, bScore)
+            return best_score
+        else:
+            bScore = 9999999999999999999999
+            for i in availableMoves(board):
+                make_move(board, "o" , i)
+                score = minimax(board, depth+1, True)
+                undoMove(board, i)
+                best_score = min(score, bScore)
+            return best_score
 
     def availableMoves(field):
-        dahfgjhsdfg = []
+            dahfgjhsdfg = []
+            for i in range(3):
+                for j in range(3):
+                    if field[i][j] == None:
+                        dahfgjhsdfg.append((i,j))
+            return dahfgjhsdfg
+
+    def findBestMove(field):
+        bestMove = None
+        bestScore = -100
         for i in range(3):
             for j in range(3):
-                if field[i][j] == None:
-                    dahfgjhsdfg.append((i,j))
-        return dahfgjhsdfg
-
-
+                move = (i,j)
+                make_move(field, "x", move)
+                moveScore = minimax(field, 0, False)
+                undoMove(field, move)
+                if moveScore > bestScore:
+                    bestScore = moveScore
+                    bestMove = move
+        return bestMove 
     def evaluateBoard(field):
         winner = None
         for i in field:
@@ -83,9 +114,35 @@ def hard_gamemode(field):
             return -10
         elif winner == "o":
             return 10
+    
         
+    def make_move(field, owner, move):
+        field[move[0]][move[1]].owner = owner
+
     def undoMove(field, move: tuple):
         field[move[0]][move[1]].owner = None
+
+    def gameOver(gitter):
+        for i in gitter:
+            if i[0].owner == i[1].owner and i[1].owner == i[2].owner and i[2].owner != None:
+                return True
+        for i in range(3):
+            if gitter[0][i].owner == gitter[1][i].owner and gitter[1][i].owner == gitter[2][i].owner and gitter[2][i].owner != None:
+                    return True
+        if gitter[0][0].owner == gitter[1][1].owner and gitter[1][1].owner == gitter[2][2].owner and gitter[2][2].owner != None:
+            return True
+        if gitter[0][2].owner == gitter[1][1].owner and gitter[1][1].owner == gitter[2][0].owner and gitter[2][0].owner != None:
+            return True
+        for i in range(3):
+            for j in range(3):
+                if gitter[i][j].owner == None:
+                    return False
+        return True
+    a = findBestMove(field)
+    j = field[a[0]][a[1]]
+    j.texture = arcade.load_texture("data/o.png")
+    j.owner = "o"
+    return field
 
 def check_for_victory(gitter):
     for i in gitter:
@@ -100,23 +157,12 @@ def check_for_victory(gitter):
         return True
     return False
 
-def sum(gitter):
-    s = 0
-    for i in gitter:
-        for j in i:
-            if j.owner != None:
-                s += 1
-    return s
-
-def ev_sum(p):
-    return "o" if p%2 == 0 else "x"
 def check_if_full(gitter):
     for i in range(3):
         for j in range(3):
             if gitter[i][j].owner == None:
                 return False
     return True
-    
 class Game(arcade.View):
     def __init__(self, bot, music):
         super().__init__()
