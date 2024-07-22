3fa80e4ea1b5c940a562b4cbd93e1b264acdbcc2
MEGADragon2.0 <106551997+MEGADragon20@users.noreply.github.com>
2024-07-22 19:37:50 +0200

    summary (required)

--- Machine Learning/projekt_juan&fer/statistik.py
+++ Machine Learning/projekt_juan&fer/statistik.py
@@ -278,7 +278,7 @@ def minmaxingOAlphaPruning(field):
                 bestScore = moveScore
                 bestMove = move
         return bestMove
-
+    
     def evaluateBoard(field):
         winner = None
         for row in field:
@@ -590,3 +590,27 @@ class W(arcade.Window):
 
 game = W()
 arcade.run()
+
+
+# tic tac toe programmieren:
+# Zeit: etwa 3-4h // Komplexität: Leicht 
+
+# "Halbwegs schlau" implementieren:
+# Zeit: etwa 1:30h // Komplexität: Leicht 
+
+# Minimax implementieren:
+# Zeit: etwa 3 h // Komplexität: Mittel
+
+# Grafiken überarbeiten:
+# Zeit: etwa 2h // komplexität: Mittel
+
+# Alpha pruning implementieren:
+# Zeit: etwa 4 h // Komplexität: Schwer 
+
+# Counter implementieren:
+# Zeit: etwa 1 h // Komplexität: Leicht
+
+# Eigene Datei schaffen und Alpha pruning gegeneinander spielen lassen:
+# Zeit: etwa 1:30 h // Komplexität: Mittel
+
+# Grafiken für statistik.py schaffen # TODO
